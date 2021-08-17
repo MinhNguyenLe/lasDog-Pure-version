dd0b3dd5d90d183e8902756ad6e68906892b322b
ZinZinNguyen <71427292+ZinZinNguyen@users.noreply.github.com>
2021-08-17 21:11:06 +0700

    delete item + done logic todo

--- action/index.js
+++ action/index.js
@@ -4,9 +4,9 @@ window.oncontextmenu = function () {
 
 let state = {
   listTodo: [],
+  countId: 0,
 };
-
-let countId = 0;
+let isNew = false;
 
 const countdown = document.querySelector(".btn-countdown");
 const todoInput = document.getElementById("todo");
@@ -14,17 +14,25 @@ const list = document.getElementById("list");
 const todo = document.querySelector(".btn-todo");
 const startCountdown = document.querySelector(".start-countdown");
 
-chrome.storage.local.get(["listTodo"], function (result) {
-  if (result.listTodo) {
-    result.listTodo.forEach((l) => {
-      list.innerHTML += l;
+function removeItem(t) {
+  t.remove();
+  state.listTodo = state.listTodo.filter((list) => list.id != t.id);
+  chrome.storage.local.set({ state }, function () {});
+}
+
+chrome.storage.local.get(["state"], function (result) {
+  if (result.state && result.state.listTodo) {
+    result.state.listTodo.forEach((l) => {
+      if (l.new) {
+        state.listTodo.push(l);
+        list.innerHTML += l.new;
+      }
     });
   }
+  if (result.state.countId) state.countId = result.state.countId;
   document.querySelectorAll(".list-todo").forEach((t) => {
-    t.addEventListener("contextmenu", () => {
-      t.remove();
-      return false;
-    });
+    console.log(t);
+    t.addEventListener("contextmenu", () => removeItem(t));
   });
 });
 
@@ -46,26 +54,26 @@ todo.addEventListener("click", () => {
 
 todoInput.addEventListener("keydown", (e) => {
   if (e.code == "Enter") {
-    let newTodo = `<div class="list-todo full-width flex row mid">
-    <input id="${countId}" class="input-child" type="checkbox"/>
-    <label class="todo-label" for="${countId}" id="todo-${countId}">${todoInput.value}</label>
+    let newTodo = `<div id="${state.countId}" class="list-todo full-width flex row mid">
+    <input id="input-${state.countId}" class="input-child" type="checkbox"/>
+    <label class="todo-label" for="input-${state.countId}" id="todo-${state.countId}">${todoInput.value}</label>
         </div>`;
 
-    state.listTodo.push(newTodo);
+    state.listTodo.push({
+      new: newTodo,
+      id: state.countId,
+    });
 
     list.innerHTML += newTodo;
 
-    const todoChild = document.querySelectorAll(".list-todo");
-    todoChild.forEach((t) => {
-      t.addEventListener("contextmenu", () => {
-        t.remove();
-        return false;
-      });
+    document.querySelectorAll(".list-todo").forEach((t) => {
+      t.addEventListener("contextmenu", () => removeItem(t));
     });
 
-    chrome.storage.local.set({ listTodo: state.listTodo }, function () {});
+    state.countId = state.countId + 1;
 
-    countId++;
     todoInput.value = "";
+
+    chrome.storage.local.set({ state }, function () {});
   }
 });
